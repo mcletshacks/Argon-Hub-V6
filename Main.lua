fbff99fa2385888d14ad78c3c1192315b5d97c52
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2024-03-24 23:47:12 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,10 @@
+--[[
+  A)aa                                      V)    vv  6)NNN  
+ A)  aa                                     V)    vv 6)      
+A)    aa  r)RRR   g)GGG   o)OOO  n)NNNN     V)    vv 6)NNNN  
+A)aaaaaa r)   RR g)   GG o)   OO n)   NN     V)  vv  6)   NN 
+A)    aa r)      g)   GG o)   OO n)   NN      V)vv   6)   NN 
+A)    aa r)       g)GGGG  o)OOO  n)   NN       V)     6)NNN  
+                      GG                                     
+                 g)GGGG                                      
+]]
